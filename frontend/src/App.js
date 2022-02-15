0702011c8580b1147a7320dd242b6a4aac76d861
Kocsis Bence <kocsisbence00@gmail.com>
2022-02-15 14:08:43 +0100

    T-2 - Alap navbar letrehozasa

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,24 @@
+import React from 'react';
+import { Navbar } from './components';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+const App = () => (
+  <div>
+    <Navbar />
+{/*     <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer /> */}
+    <div className='bg-gray-400 w-full h-screen'></div>
+  </div>
+  
+);
 
 export default App;

fa320218954f82aaa1e89b6f8a9e5b69f98840be
lermand7 <xarma007@gmail.com>
2022-02-20 19:55:27 +0100

    ikon

--- native/App.js
+++ native/App.js
@@ -1,7 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, Image, Button } from 'react-native';
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerItem } from '@react-navigation/drawer';
 import Navigator from './Routes/Stack';
+
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
+  return (
+    <Drawer.Navigator
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+    >
+      <Drawer.Screen name="Feed" component={Feed} />
+      <Drawer.Screen name="Notifications" component={Notifications} />
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
   const [name, onChangeName] = React.useState("");
@@ -9,6 +23,8 @@ export default function App() {
   const [login, onPressLogin] = React.useState("");
 
   return (
-    <Navigator />
+    <Navigator>
+      <MyDrawer />
+    </Navigator>
   );
 }
--- native/Routes/Stack.js
+++ native/Routes/Stack.js
@@ -1,14 +1,28 @@
+import Icon from 'react-native-vector-icons/FontAwesome';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import Login from '../Views/Login';
 import Home from '../Views/Home';
 
+
 const screens = {
     Login: {
-        screen: Login
+        screen: Login,
+        navigationOptions: {
+            headerShown: false,
+        }
     },
     Home: {
-        screen: Home
+        screen: Home,
+        navigationOptions: {
+            headerTintColor: 'azure',
+            headerStyle: { backgroundColor: 'grey', elevation: 0, shadowOpacity: 0, borderBottomWidth: 0 },
+            headerBackTitleVisible: true,
+            headerBackTitle: 'LOGOUT',
+            headerLeft: () => <Icon name='navicon' size={30} color='azure' onPress={() => { goBack() }} style={{ marginLeft: 10 }} />,
+            headerTitle: 'SZERELLEK',
+            headerTitleAlign: 'center',
+        }
     }
 };
 

efc42d8847cdeedd2f80c8f137cc3d6f32efa52b
Spntax <varbiro99@gmail.com>
2022-04-01 18:32:11 +0200

    Revert "Category?? nemtudom"
    This reverts commit d72cdac853aaceef8b6aad44ee9bcee48c40a7da.

--- src/DataModells/Category.model.ts
+++
@@ -1,6 +0,0 @@
-import { Schema, Document } from 'mongoose';
-
-export interface ICategoryData extends Document {
-    Name:string;
-    Parent: string;
-}
--- src/Routes/publicR.ts
+++ src/Routes/publicR.ts
@@ -1,13 +1,11 @@
 import { UserDbController } from './../DataBase/User.controller';
 import { DeviceDbController } from './../DataBase/Device.controller';
-import { CategoryDbController } from './../DataBase/Category.controller';
 import { Router } from "express";
 import Auth from '../Auth/Auth';
 
 const router = Router();
 const userController = new UserDbController();
 const DeviceController = new DeviceDbController();
-const CategoryController = new CategoryDbController();
 const auth = new Auth();
 //Publikus útvonalak
 router.post("/login", userController.login);
@@ -15,5 +13,4 @@ router.post("/register", userController.registration);
 router.post("/device",[auth.verifyToken], DeviceController.addDevice);
 router.get("/device", DeviceController.getDeviceById);
 router.get("/devices", DeviceController.getDevices);
-router.post("/category", CategoryController.addCategory);
 export default router;
